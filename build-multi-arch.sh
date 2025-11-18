2eec93a58313f774dea3c8c2fc6400bd65cefb0c
ibuetler <ivan.buetler@hacking-lab.com>
2025-11-18 14:03:09 +0100

    update

--- build-multi-arch.sh
+++ build-multi-arch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/ubuntu-base-hl:latest -f Dockerfile . --push
+docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/ubuntu-base-hl:$1 -f Dockerfile . --push
+docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/ubuntu-base-hl:$1.0 -f Dockerfile . --push
 docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/ubuntu-base-hl:18.04 -f Dockerfile.18.04 . --push
 docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/ubuntu-base-hl:22.04 -f Dockerfile.22.04 . --push
